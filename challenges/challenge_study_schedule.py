2725440919695bc047693ee8b8a6b801fcc4d257
Thiago Omoto <thi.omoto@gmail.com>
2023-06-01 19:23:53 -0300

    criando algoritmo de busca

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,13 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if type(target_time) != int:
+        return None
+
+    frequencia = 0
+    for students_time in permanence_period:
+        (inicio, final) = students_time
+        if type(inicio) != int or type(final) != int:
+            return None
+        if inicio <= target_time <= final:
+            frequencia += 1
+
+    return frequencia
