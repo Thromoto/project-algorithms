79d54181695cb300e4e9227f1f2ea2c8ebfc3891
Thiago Omoto <thi.omoto@gmail.com>
2023-06-03 17:03:08 -0300

    testes no arq encrypt_message

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,13 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError, match='tipo inválido para key'):
+        encrypt_message('xablau', 'str')
+    with pytest.raises(TypeError, match='tipo inválido para message'):
+        encrypt_message(9, 8)
+    assert encrypt_message("xablau", 9) == "ualbax"
+    assert encrypt_message("xablau", -1) == "ualbax"
+    assert encrypt_message("xablau", 3) == "bax_ual"
+    assert encrypt_message("xablau", 4) == "ua_lbax"
