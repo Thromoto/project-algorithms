2e97fd079ca3e462db3935fdba296c2712c3d7bc
Thiago Omoto <thi.omoto@gmail.com>
2023-06-04 17:47:46 -0300

    cria algoritmo que verifica se a palavra é palíndromo

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,13 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if word == "":
+        return False
+    if low_index >= high_index:
+        return True
+    elif word[low_index] != word[high_index]:
+        return False
+    else:
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+
+
+# print(is_palindrome_recursive("osso", 0, 3))
+# print(is_palindrome_recursive("reviver", 0, 6))
