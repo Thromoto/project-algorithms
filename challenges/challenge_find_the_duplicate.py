856bbb9f22852b9758d35224185468ec583b34d4
Thiago Omoto <thi.omoto@gmail.com>
2023-06-05 12:26:08 -0300

    cria algoritmo de busca

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,20 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    nums_ordenado = sorted(nums)
+    n = len(nums_ordenado)
+
+    if (len(nums) <= 1
+            or any(type(num) == str for num in nums)
+            or any(num < 0 for num in nums)):
+        return False
+    if len(nums) == 2 and nums[0] != nums[1]:
+        return False
+
+    for i in range(1, n):
+        if nums_ordenado[i] == nums_ordenado[i - 1]:
+            return nums_ordenado[i]
+
+
+# nums = [1, 3, 4, 2, 2]
+# nums = [1, 3, -4, 2, 2]
+# numeros = find_duplicate(nums)
+# print(numeros)
